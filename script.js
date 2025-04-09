dd13da5489312e290cf310cd051be34972a6648d
OK946 <owen.kolbert24@student.cart.org>
2025-04-09 13:02:52 -0700

    Created a palindrome checker

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById(`checker`).addEventListener(`click`, ()=>{
+    let word = document.getElementById(`getText`).value.toLowerCase();
+    for(let i = 0; i < word.length; i++){
+        if(word[0 + i] !== word[word.length - 1 - i] ){
+            return document.getElementById(`display`).innerText = `False: Not a Palindrome`;
+        } 
+    }
+    return document.getElementById(`display`).innerText = `True: A Palindrome`;
+});
