a168496cba58565a7cf3425d95e091ac40a3378f
OK946 <owen.kolbert24@student.cart.org>
2025-04-09 13:10:37 -0700

    Updated to remove spaces

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById(`checker`).addEventListener(`click`, ()=>{
-    let word = document.getElementById(`getText`).value.toLowerCase();
+    let word = document.getElementById(`getText`).value.toLowerCase().split(` `).join(``);
     for(let i = 0; i < word.length; i++){
         if(word[0 + i] !== word[word.length - 1 - i] ){
             return document.getElementById(`display`).innerText = `False: Not a Palindrome`;
